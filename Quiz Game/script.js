0d5a90a088667f7d112238e66ad937fec8bfde65
Rubie-Eurickah <eurickahmatata345@gmail.com>
2023-05-12 15:16:10 +0300

    Initial Commit

---
+++ Quiz Game/script.js
@@ -0,0 +1,62 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('questions-container')
+
+const shuffledQuestion, currentQuestionIndex 
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestion = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [
+            { answerText: 'New York', isCorrect: false },
+            { answerText: 'London', isCorrect: false },
+            { answerText: 'Paris', isCorrect: true },
+            { answerText: 'Dublin', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [
+            { answerText: 'Jeff Bezos', isCorrect: false },
+            { answerText: 'Elon Musk', isCorrect: true },
+            { answerText: 'Bill Gates', isCorrect: false },
+            { answerText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The iPhone was created by which company?',
+        answerOptions: [
+            { answerText: 'Apple', isCorrect: true },
+            { answerText: 'Intel', isCorrect: false },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many Harry Potter books are there?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '7', isCorrect: true },
+        ],
+    },
+];
